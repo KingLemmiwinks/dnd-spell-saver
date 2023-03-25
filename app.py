fc228ade59cd4880c231d5ca9843ccfccb0d4c9b
Tony <lemmiwinks@DESKTOP-OBISGDK.localdomain>
2023-03-25 10:02:15 -0400

    add heroku psql url

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ CURR_USER_KEY = "curr_user"
 app = Flask(__name__)
 app.app_context().push()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///postgresql-asymmetrical-78818'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL','postgresql:///capstone_1_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', '177013')
