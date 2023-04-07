a869906cd65fd4c68c8784d1caf3bb1d5219b51b
Tony <lemmiwinks@DESKTOP-OBISGDK.localdomain>
2023-04-07 10:09:21 -0400

    Updated uri

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ uri = os.environ.get('DATABASE_URL')
 if uri and uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = uri
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
