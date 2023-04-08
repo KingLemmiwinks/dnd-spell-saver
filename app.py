f47b2ebb971fc23bc8b45e8009df03dea83b07de
Tony <lemmiwinks@DESKTOP-OBISGDK.localdomain>
2023-04-08 10:50:14 -0400

    Updated uri

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 import requests, json, os
 from flask import Flask, request, redirect, render_template, flash, session, g
+from flask_debugtoolbar import DebugToolbarExtension
 from forms import RegisterForm, LoginForm
 from models import db, connect_db, User, Likes
 from sqlalchemy.exc import IntegrityError
@@ -12,9 +13,12 @@ CURR_USER_KEY = "curr_user"
 app = Flask(__name__)
 app.app_context().push()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = ('postgresql://clhkwoifscttqr:bf6607274b97b4f160290d76dfb50ff4fafc238a0a3c03f9bcafd8f6c0561744@ec2-3-234-204-26.compute-1.amazonaws.com:5432/dbqfkqhan460ai')
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://clhkwoifscttqr:bf6607274b97b4f160290d76dfb50ff4fafc238a0a3c03f9bcafd8f6c0561744@ec2-3-234-204-26.compute-1.amazonaws.com:5432/dbqfkqhan460ai"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
 
